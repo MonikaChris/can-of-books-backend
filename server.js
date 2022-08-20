7829dfccc587ae495745a0f0d1c046482e44156a
Monika <monidavies@gmail.com>
2022-08-20 15:51:54 -0700

    Make schema, model, mongoose

--- server.js
+++ server.js
@@ -3,6 +3,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_CONNECTION);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Mongoose is connected to Atlas!');
+});
 
 const app = express();
 app.use(cors());
